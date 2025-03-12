efda7968d8c8d3da7b7ea03ce18f3d193112d0f5
MohamedGasser15 <madagasser15@gmail.com>
2025-03-12 05:38:37 +0200

    feat: Add RegisterViewModel and LoginViewModel for authentication - Created `RegisterViewModel` to handle user registration data. - Created `LoginViewModel` to manage user login inputs.

---
+++ Clothes_Models/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Clothes_Models.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required]
+        [Display(Name = "Confirm Password")]
+        [System.ComponentModel.DataAnnotations.Compare("Password", ErrorMessage = "Password and Confirm Password must match")]
+        public string ConfirmPassword { get; set; }
+        public IEnumerable<SelectListItem>? RoleList { get; set; }
+        [Display(Name = "Role")]
+        public string RoleSelected { get; set; }
+    }
+}
