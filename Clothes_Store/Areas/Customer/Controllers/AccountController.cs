143d88a8ab46e2b12aa23228fc494387fe068773
MohamedGasser15 <madagasser15@gmail.com>
2025-03-12 05:30:42 +0200

    eat: Add AuthController with GET methods for Login and Register views - Implemented `AccountController` to handle authentication-related views. - Added `Login` and `Register` GET methods returning corresponding views.

---
+++ Clothes_Store/Areas/Customer/Controllers/AccountController.cs
@@ -0,0 +1,40 @@
+﻿using Clothes_DataAccess.Data;
+using Clothes_Models.Models;
+using Clothes_Utilities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clothes_Store.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly ApplicationDbContext _db;
+
+
+        public AccountController(UserManager<ApplicationUser> userManager,
+                                SignInManager<ApplicationUser> signInManager,
+                                RoleManager<IdentityRole> roleManager, ApplicationDbContext db)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+            _db = db;
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+
+    }
+}
