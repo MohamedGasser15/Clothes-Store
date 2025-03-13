77d11abde31b6d3210a2a21b1517601d296fd795
MohamedGasser15 <madagasser15@gmail.com>
2025-03-13 07:49:46 +0200

    feat: add Upsert GET and POST actions in ProductController
    - Implemented Upsert GET action to load product data for editing
    - Added Upsert POST action to handle product creation and updates
    - Ensured validation and model binding for product data
    - Prepared for integration with the Product view

--- Clothes_Store/Areas/Admin/Controllers/ProductController.cs
+++ Clothes_Store/Areas/Admin/Controllers/ProductController.cs
@@ -1,6 +1,8 @@
 ﻿using Clothes_DataAccess.Data;
 using Clothes_Models.Models;
+using Clothes_Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Clothes_Store.Areas.Admin.Controllers
 {
@@ -10,7 +12,7 @@ namespace Clothes_Store.Areas.Admin.Controllers
         private readonly ApplicationDbContext _db;
         private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public ProductController(ApplicationDbContext db ,IWebHostEnvironment webHostEnvironment)
+        public ProductController(ApplicationDbContext db, IWebHostEnvironment webHostEnvironment)
         {
             _db = db;
             _webHostEnvironment = webHostEnvironment;
@@ -20,6 +22,60 @@ namespace Clothes_Store.Areas.Admin.Controllers
         {
             List<Product> objList = _db.Products.ToList();
             return View(objList);
+        }
+
+
+        public IActionResult Upsert(int? id)
+        {
+            ProductViewModel obj = new();
+            obj.BrandList = _db.Brands.Select(i => new SelectListItem
+            {
+                Text = i.Brand_Name,
+                Value = i.Brand_Id.ToString()
+            });
+            obj.CategoryList = _db.Categories.Select(i => new SelectListItem
+            {
+                Text = i.Category_Name,
+                Value = i.Category_Id.ToString()
+            });
+            if (id == null || id == 0)
+            {
+                return View(obj);
+            }
+            obj.Product = _db.Products.FirstOrDefault(u => u.Product_Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Upsert(ProductViewModel obj, IFormFile? file)
+        {
+            string wwwRootPath = _webHostEnvironment.WebRootPath;
+            if (file != null)
+            {
+                string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                string productPath = Path.Combine(wwwRootPath, @"img");
+
+                using (var fileStream = new FileStream(Path.Combine(productPath, fileName), FileMode.Create))
+                {
+                    file.CopyTo(fileStream);
+                }
+                obj.Product.imgUrl = @"\img\" + fileName;
+            }
+            if (obj.Product.Product_Id == 0)
+            {
+                await _db.Products.AddAsync(obj.Product);
+            }
+            else
+            {
+                _db.Products.Update(obj.Product);
+            }
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
     }
 }
