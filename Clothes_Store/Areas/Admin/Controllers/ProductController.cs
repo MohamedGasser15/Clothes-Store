8e574f2022ee32170a2a814180ce88d27401cee9
MohamedGasser15 <madagasser15@gmail.com>
2025-03-13 06:45:28 +0200

    feat: add ProductController with Index action
    - Created ProductController to manage product-related operations
    - Added Index action to display a list of products
    - Ensured proper routing and integration with the view
    - Prepared the controller for future CRUD operations

---
+++ Clothes_Store/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using Clothes_DataAccess.Data;
+using Clothes_Models.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clothes_Store.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public ProductController(ApplicationDbContext db ,IWebHostEnvironment webHostEnvironment)
+        {
+            _db = db;
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            List<Product> objList = _db.Products.ToList();
+            return View(objList);
+        }
+    }
+}
