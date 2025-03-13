7c7b6849f6d099b1cceccce5c086fae2d1ce2304
MohamedGasser15 <madagasser15@gmail.com>
2025-03-13 06:59:16 +0200

    feat: add ProductViewModel
    - Created ProductViewModel to structure product data
    - Ensured proper data encapsulation for product representation
    - Prepared for integration with the Product view and controller

---
+++ Clothes_Models/ViewModels/ProductViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Clothes_Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Clothes_Models.ViewModels
+{
+    public class ProductViewModel
+    {
+        public Product Product { get; set; }
+        public IEnumerable<SelectListItem> BrandList { get; set; }
+        public IEnumerable<SelectListItem> CategoryList { get; set; }
+
+    }
+}
