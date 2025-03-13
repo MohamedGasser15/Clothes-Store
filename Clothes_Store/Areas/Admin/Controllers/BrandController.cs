e55cc9fc66d47218607293501d77b8f62b9bc7ef
MohamedGasser15 <madagasser15@gmail.com>
2025-03-13 08:19:07 +0200

    feat: add Upsert GET and POST actions in BrandController
    - Implemented Upsert GET action to load brand data for editing
    - Added Upsert POST action to handle brand creation and updates
    - Ensured validation and model binding for brand data
    - Prepared for integration with the Brand view

--- Clothes_Store/Areas/Admin/Controllers/BrandController.cs
+++ Clothes_Store/Areas/Admin/Controllers/BrandController.cs
@@ -1,6 +1,7 @@
 ﻿using Clothes_DataAccess.Data;
 using Clothes_DataAccess.Migrations;
 using Clothes_Models.Models;
+using Clothes_Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Clothes_Store.Areas.Admin.Controllers
@@ -19,6 +20,36 @@ namespace Clothes_Store.Areas.Admin.Controllers
             List<Brand> objList = _db.Brands.ToList();
             return View(objList);
         }
+        public IActionResult Upsert(int? id)
+        {
+            Brand obj = new();
+            if (id == null || id == 0)
+            {
+                return View(obj);
+            }
+            obj = _db.Brands.FirstOrDefault(i => i.Brand_Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Upsert(Brand obj)
+        {
+
+                if (obj.Brand_Id == 0)
+                {
+                    await _db.Brands.AddAsync(obj);
+                }
+                else
+                {
+                    _db.Brands.Update(obj);
+                }
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+        }
     }
 }
