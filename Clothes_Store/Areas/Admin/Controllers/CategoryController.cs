2a85a1d7b317dcbfc36a06d467b0414152e842d9
MohamedGasser15 <madagasser15@gmail.com>
2025-03-13 08:30:46 +0200

    feat: add Upsert GET and POST actions in CategoryController
    - Implemented Upsert GET action to load category data for editing
    - Added Upsert POST action to handle category creation and updates
    - Ensured validation and model binding for category data
    - Prepared for integration with the Category view

--- Clothes_Store/Areas/Admin/Controllers/CategoryController.cs
+++ Clothes_Store/Areas/Admin/Controllers/CategoryController.cs
@@ -19,5 +19,33 @@ namespace Clothes_Store.Areas.Admin.Controllers
             List<Category> objList = _db.Categories.ToList();
             return View(objList);
         }
+        public IActionResult Upsert(int? id)
+        {
+            Category obj = new();
+            if (id == null || id == 0)
+            {
+                return View(obj);
+            }
+            obj = _db.Categories.FirstOrDefault(i => i.Category_Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        public async Task<IActionResult> Upsert(Category obj)
+        {
+            if (obj.Category_Id == 0)
+            {
+                await _db.Categories.AddAsync(obj);
+            }
+            else
+            {
+                _db.Categories.Update(obj);
+            }
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
