cf920c6d98e263b08e2bb298ecbb04a879798bb6
MohamedGasser15 <madagasser15@gmail.com>
2025-03-13 07:47:59 +0200

    chore: update MVC package to the latest version
    - Upgraded ASP.NET MVC package for improved performance and security
    - Ensured compatibility with existing controllers and views
    - Verified that all features continue to function correctly after the update

--- Clothes_Models/ViewModels/ProductViewModel.cs
+++ Clothes_Models/ViewModels/ProductViewModel.cs
@@ -1,10 +1,10 @@
 ﻿using Clothes_Models.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Web.Mvc;
 
 namespace Clothes_Models.ViewModels
 {
--- Clothes_Models/ViewModels/RegisterViewModel.cs
+++ Clothes_Models/ViewModels/RegisterViewModel.cs
@@ -1,10 +1,10 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Web.Mvc;
 
 namespace Clothes_Models.ViewModels
 {
