35e855f890be4890667fe778d31dde3919424337
MohamedGasser15 <madagasser15@gmail.com>
2025-03-13 08:08:57 +0200

    feat: add BrandController
    - Created BrandController to manage brand-related operations
    - Added initial actions for listing, creating, updating, and deleting brands
    - Ensured proper routing and integration with the views
    - Prepared for future enhancements like validation and UI improvements

---
+++ Clothes_Store/Areas/Admin/Controllers/BrandController.cs
@@ -0,0 +1,22 @@
+﻿using Clothes_DataAccess.Data;
+using Clothes_DataAccess.Migrations;
+using Clothes_Models.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clothes_Store.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BrandController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public BrandController(ApplicationDbContext db)
+        {
+            db = _db;
+        }
+        public IActionResult Index()
+        {
+            List<Brand> objList = _db.Brands.ToList();
+            return View(objList);
+        }
+    }
+}
