9fd2e53e9dde051d3092eccec44ed1da0cfe5a57
MohamedGasser15 <madagasser15@gmail.com>
2025-03-13 08:24:56 +0200

    feat: add CategoryController
    - Created CategoryController to manage category-related operations
    - Added initial actions for listing, creating, updating, and deleting categories
    - Ensured proper routing and integration with the views
    - Prepared for future enhancements like validation and UI improvements

---
+++ Clothes_Store/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using Clothes_DataAccess.Data;
+using Clothes_Models.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clothes_Store.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objList = _db.Categories.ToList();
+            return View(objList);
+        }
+    }
+}
