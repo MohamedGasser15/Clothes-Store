0bce4eb36a575369353468106f2c6957e63e9a79
MohamedGasser15 <madagasser15@gmail.com>
2025-03-13 19:21:15 +0200

    feat: add UserController for user management
    - Created UserController to handle user-related operations
    - Added initial actions for listing, creating, updating, and deleting users
    - Ensured proper routing and integration with views
    - Prepared for future enhancements like authentication and role management

---
+++ Clothes_Store/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using Clothes_DataAccess.Data;
+using Clothes_Models.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clothes_Store.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ApplicationDbContext _db;
+
+        public UserController(UserManager<ApplicationUser> userManager  , ApplicationDbContext db)
+        {
+            _userManager = userManager;
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
