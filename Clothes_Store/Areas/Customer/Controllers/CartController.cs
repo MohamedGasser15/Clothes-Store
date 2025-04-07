a17f5e1643165b39fc4099358b3098fb0ff7b35e
MohamedGasser15 <madagasser15@gmail.com>
2025-04-07 19:36:50 +0200

    feat: Add CartController to manage shopping cart operations
    - Created CartController to handle cart-related actions.
    - Supports viewing cart, adding/removing items, and updating quantities.
    - Integrates with CartViewModel and ApplicationUser for personalized experience.

---
+++ Clothes_Store/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,41 @@
+﻿using Clothes_DataAccess.Data;
+using Clothes_Models.Models;
+using Clothes_Models.ViewModels;
+using Microsoft.AspNetCore.Components.RenderTree;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Clothes_Store.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class CartController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public CartController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            var cartItems = await _context.CartItems
+                .Include(ci => ci.Product)
+                .Where(ci => ci.UserId == user.Id)
+                .ToListAsync();
+
+            var viewModel = new CartViewModel
+            {
+                Items = cartItems
+            };
+
+            return View(viewModel);
+        }
+    }
+}
