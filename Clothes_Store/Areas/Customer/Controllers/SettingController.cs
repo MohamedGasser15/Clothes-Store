f0ccc0032a47f6f5f914b957ebf8f326430e16f4
MohamedGasser15 <madagasser15@gmail.com>
2025-04-11 00:41:12 +0200

    feat: Add SettingController
    - Introduced SettingController to handle routing and logic for user settings section
    - Intended to serve views like security, devices, orders, payments, and other preferences

---
+++ Clothes_Store/Areas/Customer/Controllers/SettingController.cs
@@ -0,0 +1,22 @@
+﻿using Clothes_DataAccess.Data;
+using Clothes_Models.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clothes_Store.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class SettingController : BaseController
+    {
+        public SettingController(ApplicationDbContext db, UserManager<ApplicationUser> userManager) : base(db, userManager)
+        {
+        }
+        public async Task<IActionResult> Index()
+        {
+            var userId = _userManager.GetUserId(User);
+            var user = await _userManager.FindByIdAsync(userId);
+
+            return View(user);
+        }
+    }
+}
