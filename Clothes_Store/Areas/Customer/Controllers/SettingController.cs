655cbf951774df1b58452b8c1e0f8bdc6c51aecb
MohamedGasser15 <madagasser15@gmail.com>
2025-04-11 17:26:11 +0200

    feat: Add Privacy view
    - Created Privacy view to display privacy-related information
    - Includes details on data handling, privacy policies, and user rights

--- Clothes_Store/Areas/Customer/Controllers/SettingController.cs
+++ Clothes_Store/Areas/Customer/Controllers/SettingController.cs
@@ -272,5 +272,9 @@ namespace Clothes_Store.Areas.Customer.Controllers
         {
             return View();
         }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
     }
 }
