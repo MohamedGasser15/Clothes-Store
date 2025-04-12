499e7bc1f64cee5648562aeab18ef4cd569508e3
MohamedGasser15 <madagasser15@gmail.com>
2025-04-12 12:35:50 +0200

    feat: Add Summary GET action to CartController
    - Retrieves user's cart items and calculates order totals
    - Initializes OrderHeader with user and order details
    - Returns populated CartViewModel to the Summary view

--- Clothes_Store/Areas/Customer/Controllers/CartController.cs
+++ Clothes_Store/Areas/Customer/Controllers/CartController.cs
@@ -1,37 +1,54 @@
 ﻿using Clothes_DataAccess.Data;
 using Clothes_Models.Models;
 using Clothes_Models.ViewModels;
+using Clothes_Utilities;
 using Microsoft.AspNetCore.Components.RenderTree;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Stripe.Checkout;
 
 namespace Clothes_Store.Areas.Customer.Controllers
 {
     [Area("Customer")]
     public class CartController : BaseController
     {
-        public CartController(ApplicationDbContext db, UserManager<ApplicationUser> userManager) : base(db , userManager)
+        public CartController(ApplicationDbContext db, UserManager<ApplicationUser> userManager) : base(db, userManager)
         {
         }
-
         [HttpGet]
         public async Task<IActionResult> Index()
         {
             var user = await _userManager.GetUserAsync(User);
-
             var cartItems = await _db.CartItems
                 .Include(ci => ci.Product)
                 .Where(ci => ci.UserId == user.Id)
                 .ToListAsync();
 
+            var (subtotal, shippingFee, tax, total) = CalculateOrderTotals(cartItems);
+
             var viewModel = new CartViewModel
             {
-                Items = cartItems
+                Items = cartItems,
+                OrderHeader = new OrderHeader
+                {
+                    OrderDate = DateTime.Now,
+                    ShippingDate = DateTime.Now.AddDays(3),
+                    Subtotal = subtotal,
+                    ShippingFee = shippingFee,
+                    Tax = tax,
+                    OrderTotal = total,
+                    Name = user.Name,
+                    PhoneNumber = user.PhoneNumber,
+                    StreetAddress = user.StreetAddress,
+                    Country = user.Country,
+                    PostalCode = user.PostalCode
+                }
             };
 
             return View(viewModel);
         }
+        
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> AddToCart(int productId, int quantity = 1)
@@ -161,6 +178,51 @@ namespace Clothes_Store.Areas.Customer.Controllers
             await _db.SaveChangesAsync();
 
             return RedirectToAction(nameof(Index));
+        }
+        [HttpGet]
+        public async Task<IActionResult> Summary()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            var cartItems = await _db.CartItems
+                .Include(ci => ci.Product)
+                .Where(ci => ci.UserId == user.Id)
+                .ToListAsync();
+
+            var (subtotal, shippingFee, tax, total) = CalculateOrderTotals(cartItems);
+
+            var viewModel = new CartViewModel
+            {
+                Items = cartItems,
+                OrderHeader = new OrderHeader
+                {
+                    ApplicationUserId = user.Id,
+                    OrderDate = DateTime.Now,
+                    ShippingDate = DateTime.Now.AddDays(3),
+                    Subtotal = subtotal,
+                    ShippingFee = shippingFee,
+                    Tax = tax,
+                    OrderTotal = total,
+                    OrderStatus = "Pending",
+                    PaymentStatus = "Pending",
+                    Name = user.Name,
+                    PhoneNumber = user.PhoneNumber,
+                    StreetAddress = user.StreetAddress,
+                    Country = user.Country,
+                    PostalCode = user.PostalCode
+                }
+            };
+
+            return View(viewModel);
+        }
+        
+        private (double Subtotal, double ShippingFee, double Tax, double Total) CalculateOrderTotals(IEnumerable<CartItem> cartItems)
+        {
+            double subtotal = (double)cartItems.Sum(i => i.Product.Product_Price * i.Quantity);
+            double shippingFee = subtotal > 100 ? 0 : 10;
+            double tax = subtotal * 0.08;
+            double total = subtotal + shippingFee + tax;
+
+            return (subtotal, shippingFee, tax, total);
         }
     }
 }
