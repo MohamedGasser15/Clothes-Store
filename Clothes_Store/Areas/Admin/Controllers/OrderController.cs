5e5dd38066bfcfa5555e12175bf2590b1abf7fd1
MohamedGasser15 <madagasser15@gmail.com>
2025-04-13 23:38:49 +0200

    feat: Add OrderController
    - Created OrderController in Customer area.
    - Prepares actions for viewing and managing user orders.

---
+++ Clothes_Store/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,28 @@
+﻿using Clothes_DataAccess.Data;
+using Clothes_Models.Models;
+using Clothes_Utilities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Clothes_Store.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.Admin)]
+    public class OrderController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public OrderController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<OrderHeader> objOrderHeaders = _db.OrderHeaders
+            .Include(a => a.ApplicationUser)
+            .ToList();
+
+            return View(objOrderHeaders);
+        }
+    }
+}
